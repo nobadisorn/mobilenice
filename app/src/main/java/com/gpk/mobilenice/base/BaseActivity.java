7816efe31c1df498cc8ac4601735e0132f73da4b
nob <adisorn.tala@gmail.com>
2018-03-10 14:21:48 +0700

    create base activity and base fragment

---
+++ app/src/main/java/com/gpk/mobilenice/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.gpk.mobilenice.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nobtingtong on 10/3/2018 AD.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/gpk/mobilenice/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.gpk.mobilenice.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by nobtingtong on 10/3/2018 AD.
+ */
+
+public class BaseFragment extends Fragment {
+}
