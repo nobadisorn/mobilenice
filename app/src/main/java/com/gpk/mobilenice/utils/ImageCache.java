eec518327525d01cd00ef473835f6e0c571c2379
nob <adisorn.tala@gmail.com>
2018-03-10 18:23:21 +0700

    create clase for image cach

--- app/src/main/java/com/gpk/mobilenice/utils/ImageCache.java
+++ app/src/main/java/com/gpk/mobilenice/utils/ImageCache.java
@@ -1,8 +1,19 @@
 package com.gpk.mobilenice.utils;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 /**
  * Created by nobtingtong on 10/3/2018 AD.
  */
 
 public class ImageCache {
+
+    public static void load(Context context ,String urlImage , ImageView imageView){
+        Glide.with(context)
+                .load(urlImage)
+                .into(imageView);
+    }
 }
