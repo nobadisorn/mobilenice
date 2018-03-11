f57e454a90b669b5ae690777b084812ffc020ebb
nob <adisorn.tala@gmail.com>
2018-03-11 13:40:28 +0700

    create favorite fragment

--- app/src/main/java/com/gpk/mobilenice/feature/main/adapter/PageAdapter.java
+++ app/src/main/java/com/gpk/mobilenice/feature/main/adapter/PageAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.gpk.mobilenice.R;
+import com.gpk.mobilenice.feature.main.fragment.favorite.FavoriteFragment;
 import com.gpk.mobilenice.feature.main.fragment.mobilelist.MobileListFragment;
 
 /**
@@ -23,7 +24,10 @@ public class PageAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return MobileListFragment.newInstant();
+        if (position == 0)
+            return MobileListFragment.newInstant();
+        else
+            return FavoriteFragment.newInstant();
     }
 
     @Override
---
+++ app/src/main/java/com/gpk/mobilenice/feature/main/fragment/favorite/FavoriteFragment.java
@@ -0,0 +1,39 @@
+package com.gpk.mobilenice.feature.main.fragment.favorite;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gpk.mobilenice.R;
+import com.gpk.mobilenice.base.BaseFragment;
+import com.gpk.mobilenice.databinding.LayoutRecycleViewBinding;
+
+/**
+ * Created by nobtingtong on 11/3/2018 AD.
+ */
+
+public class FavoriteFragment extends BaseFragment {
+
+    private LayoutRecycleViewBinding binding;
+
+    public static FavoriteFragment newInstant() {
+        FavoriteFragment fragment = new FavoriteFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater , R.layout.layout_recycle_view , container , false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
